21dd64c7aad33c33b720f254f7c16c455defd5a2
marekkonieczny <33019067+marekkonieczny@users.noreply.github.com>
2018-01-20 19:25:13 +0100

    Update Listy2.java

--- Listy2/src/Listy2.java
+++ Listy2/src/Listy2.java
@@ -20,7 +20,7 @@ class List
         first = null; //odnosnik do pierwszego elementu, wazne
     }
     
-    Value wstaw(String s, Value v) 
+public void  wstaw(String s) 
     {
         Value l;
         if(first == null)
@@ -30,38 +30,33 @@ class List
         }
         else
         {
-            l = new Value(s, v.next);
-            v.next = l;
+			l = new Value(s, first);
+            first = l;
         }
  
-        return l;
     }
  
 public   void drukuj() 
     {
         Value f = first;
+        System.out.print("Wezly:\n");
         while(f != null)
         {
             System.out.print(f.value + "  "); //wypisujemy listę
             f = f.next;
         }
+        System.out.print("\n");
     }
     
-    
+
+
 public    void kasuj() 
-    {
-        Value f = first;
-        Value prev = null;
-        while(f != null)
-        {
-            if (f != null)
-                if (prev == null)
-                    first = f.next;
-                else
-                    prev.next = f.next;
-        }
-        System.gc(); // - wincyj nie mogę zrobić :>
-    }
+{
+
+    if (first != null)
+    	first = null;
+    System.gc(); 
+}
 
 
 public    void usun(String s)
@@ -97,133 +92,36 @@ public   String szukaj(String s)
          	
     }
     
-    public   void bezpowtorzen() 
+    public void bezpowtorzen() 
     {
-        Value f = first;
-        //robie pusta nowa liste
+    	List kopia = new List();
+    	Value f = first;
         while(f != null)
         {
-            //zapisuje element jako element nowej listy po ifie czy sie nie powtarza (w petli dla kazdego argumentu drugiej listy)
+        	if (kopia.szukaj(f.value)==null)
+        	kopia.wstaw(f.value);
             f = f.next;
         }
+        kopia.drukuj();
     }
     
-/*public    void scal(Value a, Value b) 
+    public void scal(List a) 
     {
-        Value f = first;
-        while(a.first != null)
-        {
-            System.out.print(f.value + "  "); //wypisujemy listę
-            f = f.next;
-        }
-    }*/
+	Value f = a.first;
+    while(f != null)
+    	{
+    	this.wstaw(f.value);
+        f = f.next;
+    	}
+    a.kasuj();
+    }
     
 }
-//*/ 
+
 
 //---------------------------------------------------------------------------------------------
 
-/*dwukierunkowa wiazana cykliczna
-class Value
-{
-    String value;
-    Value next;
-    Value prev;
- 
-    Value(String s, Value Next, Value Prev)
-    {
-        value = s;
-        next = Next;
-        prev = Prev;
-    }
-}
- 
-class List
-{
-    Value first;
-    Value last;
- 
-    List()
-    {
-        first = null;
-        last = null;
-    }
- 
-    Value add(String s, Value v)
-    {
-        Value l;
-        if(first == null)
-        {
-            l = new Value(x, null, null);
-            first = l;
-            first.prev = last;
-            last = l;
-            last.next = first;
-        }
-        else
-        {
-            l = new Value(s, v.next, v);
- 
-            if(l.next != null)
-                v.next.prev = l;
-            else
-            {
-                last = l;
-                last.next = first;
-            }
- 
-            v.next = l;
-        }
- 
-        return l;
-    }
- 
-    void delete(String s)
-    {
-        Value f = first;
-        Value prev = null;
- 
-        while (f != null && f.value != s) {
-            prev = f ;
-            f = f.next;
-        }
- 
-        if (f != null)
-            if (prev == null)
-            {
-                first = f.next;
-                first.prev = last;
-            }
-            else
-            {
-                prev.next = f.next;
- 
-                if(f.next == null)
-                {
-                    last = prev;
-                    last.next = first;
-                }
-                else
-                    f.next.prev = prev;
-            }
-    }
- 
-    int searchValue(String s)
-    {
-        Value f = first;
-        
-        if(f.value == s)
-            return f.value;
-        else
-            f = f.next;
- 
-        while(f != null && f.value != s || f != first)
-            f = f.next;
- 
-        return f.value;
-    }
-}
-//*/
+
 
 //---------------------------------------------------------------------------------------------
 
@@ -232,28 +130,34 @@ public class Listy2 {
     public static void main(String[] args) {
  
         List l1 = new List(); //tworzymy listę
-        List l2 = new List(); //tworzymy listę
-       Value v1 = null;
-v1 = 
-        l1.wstaw("a",v1); 
-		l1.wstaw("11",v1); 
-        l1.wstaw("13",v1); 
-        l1.wstaw("11",v1);  
-        System.out.println(l1.szukaj("11"));
-        l1.usun("11"); 
-        l1.drukuj();
-     //   l1.kasuj();
-        l1.drukuj();
-       
-        Value v2 = null;
-v2 = 
-        l2.wstaw("c",v2); 
-        l2.wstaw("cc1",v2); 
-        l2.wstaw("1c",v2); 
-    //    l2.wstaw("1c",v2);
+       //l1.wstaw("a",v1); 
+        l1.wstaw("a");
+        l1.wstaw("a");
+        System.out.print("Szukam a w l1, wyswietli null albo nr wezla (od 0)\n");
+		System.out.println(l1.szukaj("a"));
+		System.out.print("Drukuje l1, wstawilem a, a\n");
+		l1.drukuj();
+        List l2 = new List();
+        l2.wstaw("b");
+        l2.wstaw("3a");
+        l2.wstaw("4");
+        l2.wstaw("3");
+        l2.usun("3a");
+        System.out.print("Drukuje l2, wstawilem b, 3a, 4, 3, usunalem 3a\n");
         l2.drukuj();
         l2.kasuj();
+        System.out.print("Drukuje l2 po skasowaniu\n");
         l2.drukuj();
+        System.out.print("Drukuje l1 po skasowaniu l2\n");
         l1.drukuj();
+        l2.wstaw("I did it");
+        l2.wstaw("ccc");
+        l1.scal(l2);
+        System.out.print("Drukuje l1 po scaleniu z l2, do ktorego dodalem I did it, ccc\n");
+        l1.drukuj();
+        System.out.print("Drukuje l2 po scaleniu do l1, powinno byc skasowane\n");
+        l2.drukuj();
+        System.out.print("Drukuje l1 bez powtorzen\n");
+        l1.bezpowtorzen();
     }
 }
